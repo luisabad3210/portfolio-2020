1215ba19f4196642bcd3e8ae472f2cffaf3946d7
Luis <luisabad3210@gmail.com>
2020-12-11 13:28:10 -0500

    adding css to post

--- src/components/create/create.js
+++ src/components/create/create.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import ProfileImg from '../images/profile-pic.png';
 import NewsFeed from './newsFeed';
-
+import OldPortfolio from '../images/oldPortfolio.png'
+import ProjectOne from '../images/projectOne.png';
+import ProjectTwo from '../images/projectTwo.png';
 
 function createNewsFeed(date, body, comments, likes) {
     return {
@@ -17,16 +19,17 @@ class Post extends Component {
     state = {
         index: null,
         response: '',
-        // IsInputFieldEmpty: 
+        isPosting: false,
         feeds: [
             createNewsFeed('Tue Feb 04 2020', 'Welcome to my portfolio page. Before you go on, this page is best viewed on a  screen width of 1500px or a 13inch MacBook Pro. Feel free to explore around by writing on my portfolio wall, liking, commenting, and even try out dark-mode in the settings. Have fun! :)', [], 0),
             createNewsFeed('Tue Feb 04 2020', 'There’s something always being added to this page like new features and projects, so you might see a few glitches here and there lol', [], 0),
+            createNewsFeed('Tue Feb 04 2020', 'Checkout my old Protfolio', [], 0),
+            createNewsFeed('Tue Feb 04 2020', <a href="http://luisabad3210.github.io/facebook/" target='_blank'><img alt='Twitter project' src={OldPortfolio} width='100%' height='220%' /></a>, [], 0),
             createNewsFeed('Tue Feb 04 2020', 'Click on the image bellow to view projects', [], 0),
-            // createNewsFeed('Tue Feb 04 2020', <a href="http://luisabad3210.github.io/twitter/"><img alt='Twitter project' src={projectOne} width='100%' height='220%' /></a>, [], 0),
-            // createNewsFeed('Tue Feb 04 2020', <a className='projectTwo' href="https://luisabad3210.github.io/calculator-app/"><img alt='Calculator project' src={projectTwo} width='50%' height='50%' /></a>, [], 0),
+            createNewsFeed('Tue Feb 04 2020', <a href="http://luisabad3210.github.io/twitter/" target='_blank'><img alt='Twitter project' src={ProjectOne} width='100%' height='220%' /></a>, [], 0),
+            createNewsFeed('Tue Feb 04 2020', <a href="https://luisabad3210.github.io/calculator-app/" target='_blank'><img alt='Calculator project' src={ProjectTwo} width='50%' height='50%' /></a>, [], 0),
         ],
     }
-
 
     render() {
         return (
@@ -36,18 +39,25 @@ class Post extends Component {
                     <div>
                         <div className='postInputContainer'>
                             <img src={ProfileImg} width='50px' height='50px'/>
-                            <input className='postInput' placeholder="What's on your mind?"
-                                onChange = {(event)=>{
+                            <input className='postInput' id='post-input' placeholder="What's on your mind?"
+                                
+                                onChange = {(event)=> {
                                     let text = event.target.value;
-                                    console.log(text)
+                                    // console.log(text)
+                                    if (text !== '') {
+                                        this.setState({isPosting: true})
+                                        // clear()
+                                        
+                                    } else {
+                                        this.setState({isPosting: false})
+                                    }
                                     this.setState({response: text});
-                                    
                                 }}
                             ></input>
                         </div>
     
                         <hr></hr>
-                        <button className='postBtn' onClick={() => {
+                        <button className={this.state.isPosting === false ? 'postBtnGrey' : 'postBtnBlue'} onClick={() => {
                             let text = this.state.response
                             if (text === '') {
                                 console.log('error')
--- src/components/create/newsFeedPost.js
+++ src/components/create/newsFeedPost.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import ProfilePic from '../images/profile-pic.png';
+// import 
 
 class NewsFeedPost extends Component {
 
@@ -11,18 +12,32 @@ class NewsFeedPost extends Component {
         return (
             <div>
                 <div className='newsFeedContainer'>
-                    <div> <img src={ProfilePic} width='35px' height='35px'/> Luis Abad created a post</div>
-                    <div>{this.props.feed.date}</div>
+                    <div className='postHeader'> 
+                        <img className='commentProfilePic' src={ProfilePic} /> 
+                        <div>
+                            <div>Luis Abad <i>created a post</i></div>
+                            <div><i>{this.props.feed.date}</i><img className='globe' src="https://static.xx.fbcdn.net/rsrc.php/v3/yA/r/yQ3UwzfyAi-.png" alt="Public"/></div>
+                        </div>
+                    </div>
                     <hr></hr>
-                    <div>{this.props.feed.body}</div>
+                    <div className='bodyFeed'>{this.props.feed.body}</div>
                     <hr></hr>
                     <div>
-                        {this.props.feed.likes}
-                            {/* likes button */}
                         <button
+                            className='likeBtn'
                             onClick = {() => {
                             this.props.magicalFunction(this.props.feed, this.props.index)}}
-                        >Like</button>
+                        >{this.props.feed.likes} Likes</button>
+                        <button className='likeBtn'
+                        onClick={()=>{
+                            let x = this.state.isCommenting;
+                            this.setState({isCommenting: !x});
+                        }}>
+                            Comment
+                        </button>
+                        <button className='likeBtn'>Share</button>
+                        {this.state.isCommenting === true ? <div><hr></hr><div className='searchContainer'><img className='commentProfilePic' src={ProfilePic}/><button className='navSearchBtn'></button><input className='navSearchBar' placeholder='Write a comment...'></input></div></div>: null}
+                        
                     </div> 
                 </div>
             </div>
